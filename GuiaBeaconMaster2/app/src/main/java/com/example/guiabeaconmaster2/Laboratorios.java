62cd37c2376083efd0045df36d02e5d87adf2e7c
Johao losa <johao.j10@gmail.com>
2019-06-19 19:47:04 -0500

    Desarrollo
    la aplicacion contiene
    - conexion a la DB
    - Conexion Beacon
    - navegacion por layout

---
+++ GuiaBeaconMaster2/app/src/main/java/com/example/guiabeaconmaster2/Laboratorios.java
@@ -0,0 +1,21 @@
+package com.example.guiabeaconmaster2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Laboratorios extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laboratorios);
+
+
+
+
+
+
+    }
+}
