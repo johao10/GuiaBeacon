a8a5fc517ccfbbe92fa070bedb9d01fac73eaac4
Ronald Obet <30287682+obetronald@users.noreply.github.com>
2019-06-21 17:41:29 -0500

    actualizando registro y login
    actualizando registro y login activitys

--- GuiaBeaconMaster2/app/src/main/java/com/example/guiabeaconmaster2/MainActivity.java
+++ GuiaBeaconMaster2/app/src/main/java/com/example/guiabeaconmaster2/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fm = getSupportFragmentManager();
-        fm.beginTransaction().replace(R.id.escenario, new LoginFragment()).commit();
+
     }
 }
